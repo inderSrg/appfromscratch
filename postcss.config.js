ab94e85890821bd68ce4206394b22e88e8aef579
Inder Singh <inder.patliya@LM-ILB-INDER.local>
2018-12-07 20:01:43 +0530

    basic structure

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+const postcssImports = require("postcss-import");
+
+module.exports ={
+    parser:"postcss",
+    plugins: [postcssImports]
+};
+
+
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    entry: path.join(__dirname, "src","public","js","main.js"),
+    output:{
+        path:path.join(__dirname, "dist"),
+        filename:"bundle.js"
+    },
+    resolve:{
+        extensions: [".jsx", ".js"]
+    },
+    mode:"development",
+    module:{
+        rules:[
+            {
+                test: /\.jsx?/,
+                exclude:/node_modules/,
+                loader : "babel-loader",
+                query:{
+                    presets:["babel-preset-react"]
+                }
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+          template: path.join(__dirname, "src", "views", "index.html")
+    })
+    ]
+}
